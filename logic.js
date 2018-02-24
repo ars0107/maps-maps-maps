a4d231a0aae51bb09f5cc04862082082fc6f9117
ars0107 <arwen.ricard@gmail.com>
2018-02-24 11:14:55 -0600

    created color bins for circle markers

--- logic.js
+++ logic.js
@@ -14,14 +14,46 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 // query url
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
+
+// selecting a list of colors to assign to different magnitudes
+var colors = ["rgb(101, 188, 241)", "rgb(1, 82, 255)", "rgb(129, 64, 233)", "rgb(245, 8, 245)", "rgb(8, 245, 134)", "rgb(245, 182, 8)"]
+
+// putting the colors and magnitudes into a bin
+function magColor(magnitude) {
+    if (magnitude <= 1) {
+        return colors[0];
+    } else if (magnitude > 1 && magnitude < 2) {
+        return colors[1];
+    } else if (magnitude > 2 && magnitude < 3) {
+        return colors[2];
+    } else if (magnitude > 3 && magnitude < 4) {
+        return colors[3];
+    } else if (magnitude > 4 && magnitude < 5) {
+        return colors[4];
+    } else {
+        return colors[5];
+    }
+};
+
+// pulling the geojson data 
 d3.json(url, function(data) {
     L.geoJson(data, {
-        style: function(feature) {
-            return {
+        // adding a map layer
+        pointToLayer: function(feature, latlng) {
+            // creating a circle marker
+            return L.circleMarker(latlng, {
+                // styling circle marker 
+                radius: feature.properties.mag * 2, /* this selects the size of the circle based on magnitude */
+                fillColor: magColor(feature.properties.mag), /* this selects a color based on magnitude */ 
                 color: "white",
-            };
-            layer.bindPopup("<h1>"+ feature.properties.title + "</h1>");
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            });
+            // adding a popup when the user clicks a circle
+            marker.bindPopup(feature.properties.mag).openPopup();
         }
+    // adding the layer to the map
     }).addTo(myMap);
 });
 
